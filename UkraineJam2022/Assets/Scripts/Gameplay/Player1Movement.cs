b8e51d879ec88fa747774a282dcc20b8296c007b
Szymon Rzosiński <szymonrzosinski@gmail.com>
2022-07-09 15:51:07 +0200

    staircase, animations WIP

--- UkraineJam2022/Assets/Scripts/Gameplay/Player1Movement.cs
+++ UkraineJam2022/Assets/Scripts/Gameplay/Player1Movement.cs
@@ -10,12 +10,16 @@ public class Player1Movement : MonoBehaviour
     [SerializeField] float _speed = 2f;
 
     Rigidbody2D _myRB;
-
+    Animator _myAnim;
     Vector2 _lastInput;
+
+    Player _myPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
+        _myPlayer = GetComponent<Player>();
+        _myAnim = GetComponent<Animator>();
         _myRB = GetComponent<Rigidbody2D>();
         InputManager.I.Player1AM.Main.Movement.performed += ProcessInput;
         InputManager.I.Player1AM.Main.Movement.canceled += ProcessInput;
@@ -29,6 +33,12 @@ public class Player1Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _myAnim.SetFloat("Horizontal",Math.Abs(_lastInput.normalized.x));
+        if(_lastInput.x<0.25f && !_myPlayer.PlayerSR.flipX)
+            _myPlayer.PlayerSR.flipX = true;
+        else if(_lastInput.x>0 && _myPlayer.PlayerSR.flipX)
+            _myPlayer.PlayerSR.flipX = false;
+        _myAnim.SetFloat("Vertical",_lastInput.normalized.y);
         _myRB.MovePosition(_myRB.position+_lastInput*_speed*Time.deltaTime);
     }
 }
